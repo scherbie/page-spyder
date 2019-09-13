231344d62546952769d89793c468b15f68641c7b
Dima Scherbie <dima.scherbie@gmail.com>
2019-09-12 20:42:19 -0400

    first commit

---
+++ utilities/database_utilities.py
@@ -0,0 +1,8 @@
+def create_database(database_path: str):
+    # TODO: generate the database
+    pass
+
+
+def save_words_to_database(database_path: str, words: list):
+    # TODO: save the words to database
+    pass
---
+++ utilities/url_utilities.py
@@ -0,0 +1,13 @@
+def load_urls_from_file(file_path: str):
+    # TODO: add the code to read the file
+    pass
+
+
+def load_page(url: str):
+    # TODO: add the code to read the url
+    pass
+
+
+def scrape_page(url: str):
+    # TODO: analyze the text
+    pass
